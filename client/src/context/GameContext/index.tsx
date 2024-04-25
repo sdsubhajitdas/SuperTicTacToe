87399d1b14a2c3e7859db643a8f95da16ecbe2c9
Subhajit Das <sdsubhajitdas98@gmail.com>
2024-04-25 09:52:16 +0530

    Clean up game room context

--- client/src/context/GameContext/index.tsx
+++ client/src/context/GameContext/index.tsx
@@ -1,6 +1,4 @@
 import { createContext, useState } from "react";
-import { useEffect } from "react";
-import { io } from "socket.io-client";
 
 type GameContextProviderProps = {
   children: React.ReactNode;
@@ -10,10 +8,6 @@ export const GameContext = createContext({});
 
 function GameContextProvider({ children }: GameContextProviderProps) {
   const [roomId, setRoomId] = useState("");
-
-  useEffect(() => {
-    // let socket = io();
-  }, []);
 
   return (
     <GameContext.Provider value={{ roomId, setRoomId }}>
